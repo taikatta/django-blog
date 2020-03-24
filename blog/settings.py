6126a2cd741cd6d6ffbc3f6ec29f9eadb8c5e17d
Boglarka Barabas <taikatta@gmail.com>
2020-03-24 16:28:10 +0000

    Typo fixed

--- blog/settings.py
+++ blog/settings.py
@@ -87,7 +87,7 @@ if "DATABASE_URL" in os.environ:
     }
 else:
     print("Postgres URL not found, using sqlite instead")
-     DATABASES = {
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
