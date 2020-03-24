985126a14540fff38a019589ae55c4b483ec4da1
Boglarka Barabas <taikatta@gmail.com>
2020-03-24 16:30:40 +0000

    ALLOWED_HOSTS fixed

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog-test-app.herokuapp.com']
+ALLOWED_HOSTS = ['django-blog-bo.herokuapp.com']
 
 
 # Application definition
